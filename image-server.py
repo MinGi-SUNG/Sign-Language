83e4ae9ba6261a50f1b093ca7fb0eb699d9a5c4c
reenact_l <77737814+jhlee343@users.noreply.github.com>
2023-06-20 00:35:38 +0900

    Update image-server.py

--- image-server.py
+++ image-server.py
@@ -1,77 +1,33 @@
 import socket
-import cv2
 import numpy
-from queue import Queue
-from _thread import *
+import cv2
 
-enclosure_queue = Queue()
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5000
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
 
-# 쓰레드 함수
-def threaded(client_socket, addr, queue):
-    print('Connected by :', addr[0], ':', addr[1])
+s = [b'\xff' * 46080 for x in range(20)]
 
-    while True:
-
-        try:
-            data = client_socket.recv(1024)
-
-            if not data:
-                print('Disconnected by ' + addr[0], ':', addr[1])
-                break
-
-            stringData = queue.get()
-            client_socket.send(str(len(stringData)).ljust(16).encode())
-            client_socket.send(stringData)
-
-        except ConnectionResetError as e:
-
-            print('Disconnected by ' + addr[0], ':', addr[1])
-            break
-
-    client_socket.close()
-
-
-def webcam(queue):
-    capture = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = capture.read()
-
-        if ret == False:
-            continue
-
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-
-        data = numpy.array(imgencode)
-        stringData = data.tostring()
-
-        queue.put(stringData)
-
-        cv2.imshow('image', frame)
-
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-
-
-HOST = '0.0.0.0'
-PORT = 5000
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
-
-print('server start')
-
-start_new_thread(webcam, (enclosure_queue,))
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output.avi', fourcc, 25.0, (640, 480))
 
 while True:
-    print('wait')
+    picture = b''
 
-    client_socket, addr = server_socket.accept()
-    start_new_thread(threaded, (client_socket, addr, enclosure_queue,))
+    data, addr = sock.recvfrom(46081)
+    s[data[0]] = data[1:46081]
 
-server_socket.close() 
+    if data[0] == 19:
+        for i in range(20):
+            picture += s[i]
+
+        frame = numpy.fromstring(picture, dtype=numpy.uint8)
+        frame = frame.reshape(480, 640, 3)
+        cv2.imshow("frame", frame)
+        out.write(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
